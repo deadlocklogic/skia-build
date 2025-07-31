94d4996ed62243180af36c72301fdbfe920e2ebf
Deadlocklogic <deadlocklogic@gmail.com>
2025-07-31 09:26:08 +0300

    install-deps-windows.ps1: fix line breaks

--- scripts/install-deps-windows.ps1
+++ scripts/install-deps-windows.ps1
@@ -3,14 +3,14 @@ Set-StrictMode -Version Latest
 Write-Host "Installing Windows dependencies..."
 
 # Install essential tools sorted alphabetically
-choco install -y \
-  cmake \
-  curl \
-  git \
-  llvm \
-  ninja \
-  python \
-  unzip \
+choco install -y `
+  cmake `
+  curl `
+  git `
+  llvm `
+  ninja `
+  python `
+  unzip `
   zip
 
 Write-Host "Windows dependencies installed."
